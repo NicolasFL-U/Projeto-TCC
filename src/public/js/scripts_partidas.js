de9b731a05239b2bfa57fda0a44e39ae749daf71
NicolasFL-U <131829311+NicolasFL-U@users.noreply.github.com>
2024-10-14 18:46:51 -0400

    Modulo 4 fix
    Por algum motivo, eu acabei deletando funções que foram feitas no módulo 3 da branch módulo 4.
    
    Esse commit só trás as funções de volta com algumas otimizações.

--- src/public/js/scripts_partidas.js
+++ src/public/js/scripts_partidas.js
@@ -1,4 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const btnAtualizar = document.getElementById('btnAtualizar');
+
+    btnAtualizar.addEventListener('click', function () {
+        // Mostrar o modal de atualização
+        $('#atualizarModal').modal('show');
+    });
+
+    fetch('/dadosUsuario')
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                console.error('Erro ao carregar os dados:', data.error);
+                return;
+            }
+
+            // Exibir os dados do usuário na página
+            document.getElementById('gameName').textContent = `${data.gameName}#${data.tagLine}`;
+            document.getElementById('profileIcon').src = `https://ddragon.leagueoflegends.com/cdn/14.20.1/img/profileicon/${data.profileIconId}.png`;
+            document.getElementById('summonerLevel').textContent = `Nível: ${data.summonerLevel}`;
+            document.getElementById('tier').textContent = data.tier;
+            document.getElementById('rank').textContent = data.rank;
+            document.getElementById('leaguePoints').textContent = `${data.leaguePoints} PDL`;
+
+            // Vitórias/Derrotas e Winrate
+            const wins = data.wins;
+            const losses = data.losses;
+            const totalPartidas = wins + losses;
+            const winrate = ((wins / totalPartidas) * 100).toFixed(1); // Calcula o winrate com 1 casa decimal
+            document.getElementById('wins').textContent = wins;
+            document.getElementById('losses').textContent = losses;
+            document.getElementById('winrate').textContent = `(${winrate}% WR)`;
+
+            // Adicionar o ícone do elo ao lado do Elo e PDL
+            const eloIconPath = `/ranked/${data.tier}.png`; // Supondo que os ícones dos elos estão na pasta "/ranks/"
+            document.getElementById('eloIcon').src = eloIconPath;
+        })
+        .catch(error => console.error('Erro ao carregar os dados do usuário:', error));
+
     // Adicionar evento ao botão de salvar VOD
     document.getElementById('salvarVodButton').addEventListener('click', function (event) {
         event.preventDefault(); // Impede o envio padrão do formulário
